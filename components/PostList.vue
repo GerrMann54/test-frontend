030485ef50ac6c9800b2daac391de101b3742c05
User <none>
2025-06-04 19:03:43 +0400

    post detail

--- components/PostList.vue
+++ components/PostList.vue
@@ -2,6 +2,7 @@
     <div>
         <div v-for="post in posts" :key="post.id">
             <h2>{{ post.title }}</h2>
+            <img :src="post.image" alt="Post Image" />
             <p>{{ post.description }}</p>
             <nuxt-link :to="`/posts/${post.id}`">Читать далее</nuxt-link>
         </div>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ post.title }}</h1>
+        <img :src="post.image" alt="Post Image" />
+        <p>{{ post.description }}</p>
+        <div v-html="post.body"></div>
+        <nuxt-link to="/posts">Назад к всем постам</nuxt-link>
+    </div>
+</template>
+
+<script setup>
+import { useAsyncData } from "nuxt/app";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const postId = route.params.id;
+const { data: post } = await useAsyncData("post", () =>
+    $fetch(`http://localhost:3001/posts/${postId}`),
+);
+</script>
+
+<style lang="scss"></style>
