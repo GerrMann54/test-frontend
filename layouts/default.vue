2d5ee1e8da0bcfe72fbd553d969d8dc0fcfc5720
User <none>
2025-06-04 17:10:14 +0400

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <header>
+            <h1>Nuxt App</h1>
+            <nav>
+                <nuxt-link to="/">Главная</nuxt-link>
+                <nuxt-link to="/posts">Все посты</nuxt-link>
+            </nav>
+        </header>
+        <NuxtPage />
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss"></style>
