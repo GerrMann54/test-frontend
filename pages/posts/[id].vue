cbd7267209b9f6b337a5f58e09342d45e475855f
User <none>
2025-06-05 16:55:29 +0400

    deletion testing

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -5,18 +5,32 @@
         <p>{{ post.description }}</p>
         <div v-html="post.body"></div>
         <nuxt-link to="/posts">Назад к всем постам</nuxt-link>
+        <button @click="deletePost">Удалить пост</button>
     </div>
 </template>
 
 <script setup>
 import { useAsyncData } from "nuxt/app";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 const postId = route.params.id;
+
 const { data: post } = await useAsyncData("post", () =>
     $fetch(`http://localhost:3001/posts/${postId}`),
 );
+
+const deletePost = async () => {
+    try {
+        await $fetch(`http://localhost:3001/posts/${postId}`, {
+            method: "DELETE",
+        });
+        router.push("/posts");
+    } catch (error) {
+        console.error("Ошибка при удалении поста:", error);
+    }
+};
 </script>
 
 <style lang="scss"></style>
